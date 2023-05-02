178f94fcf3352448ebd3998e77cd5969a622a65c
Laurent PEREIRA <laurent.pereira1@gmail.com>
2023-05-02 10:15:51 -0400

    Step2

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,6 @@
 import { convertToRoman } from "../services/converter";
 import { numberSchema } from "../validators";
+import { clients } from "../routes";
 
 export const convert = (req, res) => {
     const { body } = req;
@@ -7,7 +8,12 @@ export const convert = (req, res) => {
     if (error) {
         return res.status(400).json(error);
     }
-    const { number } = body;
+    const { number, session } = body;
     const result = convertToRoman(number);
-    res.status(200).json({ result });
+    
+    clients.filter((client) => client.id === session).find(client => client.response.write(`data: ${JSON.stringify({id: client.id, result: result})}\n\n`));
+    
+    console.log(`Send message to client ${session}`)
+
+    return res.status(200).json({ success: true});
 }
--- validators/index.js
+++ validators/index.js
@@ -2,4 +2,5 @@ import Joi from 'joi';
 
 export const numberSchema = Joi.object({
     number: Joi.number().integer().min(1).max(100).required(),
+    session: Joi.string().required()
 });
